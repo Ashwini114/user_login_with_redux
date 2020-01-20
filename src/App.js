57c6d19a16fec526862345eb573a101db6c4a932
Ashwini Ravi Nair <itsmeashwini1104@gmail.com>
2020-01-20 11:43:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import { connect } from "react-redux";
+import { changeLoginStatus } from "./js/actions";
 
-function App() {
+/** mapping state values to props */
+
+const mapStateToProps = state => {
+  return {
+    credentials: state.loginReducer.credentials,
+    login_status: state.loginReducer.login_status,
+    users : state.userReducer.users
+   };
+};
+/* Dispatching actions to props */
+function mapDispatchToProps(dispatch) {
+  return {
+    changeLoginStatus: status => dispatch(changeLoginStatus(status))
+  };
+}
+
+const App = ({credentials,login_status,changeLoginStatus,users}) => {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <Switch>
+     <Route path="/dashboard">
+     <Dashboard users={users} login_status={login_status}/>
+      </Route>
+     <Route path="/">
+          <Login credentails={credentials} changeLoginStatus={changeLoginStatus} />
+      </Route>
+    </Switch>   
     </div>
   );
 }
 
-export default App;
+const AppContainer = connect(mapStateToProps,mapDispatchToProps)(App)
+export default AppContainer;
